4e5bfd846050b68a59497d2b215d875643f757c4
2akhila2 <akhilaammu582@gmail.com>
2024-11-29 15:10:38 +0530

    Add files via upload

---
+++ muliplication.py
@@ -0,0 +1,5 @@
+n=int(input("enter the number:"))
+limit=int(input("enter the limit of the number:"))
+for i in range(1,limit+1):
+	multi=i*n
+	print(i,"*",n,"=",multi)
---
+++ readandprint.py
@@ -0,0 +1,4 @@
+a=int(input("please enter your limit:"))
+for i in range(1,a+1):
+	b=int(input("enter number:".format(i)))
+	print (b)
